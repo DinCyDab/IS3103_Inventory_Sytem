e21e19a86f970615f6aa9607c4686cc5567e7abc
Dino Cyrano Dabon <19103291@usc.edu.ph>
2025-09-24 18:31:08 +0800

    Update dashboard.php

--- mvc/view/dashboard.php
+++ mvc/view/dashboard.php
@@ -1,7 +1,31 @@
 <?php 
+    // require_once __DIR__ . "/../controller/dashboardcontroller.php";
     class Dashboard{
         public function render(){
-            echo "Dashboard View";
+            $this->graph();
+        }
+
+        public function graph(){
+            ?>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+                <canvas id="myChart" style="width:100%;max-width:700px"></canvas>
+                
+                <script>
+                    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+                    var yValues = [55, 49, 44, 24, 15];
+                    var barColors = ["red", "green","blue","orange","brown"];
+
+                    new Chart("myChart", {
+                        type: "bar",
+                        data: {
+                            labels: xValues,
+                            datasets: [{
+                                data: yValues
+                            }]
+                        }
+                    });
+                </script>
+            <?php
         }
     }
 ?>
