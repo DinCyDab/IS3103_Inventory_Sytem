6c191c311ac313a4b1768234bb07130e98cf08b9
James Ninobert Z. Galon <jamesgalon06@gmail.com>
2025-11-23 22:11:44 +0800

    November 23, 2025
    No backend

--- mvc/view/navigation.php
+++ mvc/view/navigation.php
@@ -1,17 +1,34 @@
 <?php 
     class Navigation{
         private $nav_items = [
-            "Dashboard",
-            "Accounts",
-            "Inventory",
-            "Sales",
-            "Reports",
-            "Settings"
+            [ "label" => "Dashboard", "icon" => "bx bx-home-alt", "view" => "dashboard" ],
+            [ "label" => "Accounts", "icon" => "bx bx-user", "view" => "accounts" ],
+            [ "label" => "Inventory", "icon" => "bx bx-package", "view" => "inventory" ],
+            [ "label" => "Sales", "icon" => "bx bx-cart", "view" => "sales" ],
+            [ "label" => "Reports", "icon" => "bx bx-line-chart", "view" => "reports" ],
+            [ "label" => "Settings", "icon" => "bx bx-cog", "view" => "settings" ],
+            [ "label" => "Logout", "icon" => "bx bx-log-out", "view" => "logout" ],
+
         ];
         public function render(){
-            foreach($this->nav_items as $view){
+            // detect active view
+            $active = $_GET["view"] ?? "dashboard";
+
+            foreach($this->nav_items as $item){
+                $view = $item['label'];
+                $icon = $item['icon'];
+                $isLogout = isset($item["logout"]) && $item["logout"] === true;
+
+                // compute link
+                $href = $isLogout ? "logout.php" : "index.php?view=" . $item["view"];
+
+                // detect active class
+                $isActive = !$isLogout && $active === $item["view"];
                 ?>
-                    <a href="<?php echo "?view=".$view?>"><?php echo $view?></a>
+                    <a href="<?= $href; ?>" class="<?= $isActive ? 'active' : ''; ?> <?= $isLogout ? 'logout-btn' : '' ?>">
+                    <i class="<?php echo $icon; ?>"></i>
+                    <span><?php echo $view; ?></span>
+                    </a>
                 <?php
             }
         }
